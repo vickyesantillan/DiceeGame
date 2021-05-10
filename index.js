ad2f40b5bce6e95a39815793907d1d0844d7be21
vickyesantillan <vesantillan@live.com>
2021-05-10 16:48:53 -0300

    adding js function, img1 change every time its refresh

---
+++ index.js
@@ -0,0 +1,9 @@
+var range = 6 * Math.random() + 1; // randoms numbers from 1.000-5.99999
+var randomNumber1 = Math.floor(range); //randoms numbers from 1-6
+//var imgRandomPath1To6 = 'images/dice' + randomNumber1 + '.png';
+
+// document.addEventListener('click',document.querySelector('.img1'))
+
+document
+  .querySelector('img1')
+  .setAttribute('src', 'images/dice' + randomNumber1 + '.png');
