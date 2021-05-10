5a8a1d5694e8a3b522eec841b9b0c60cf45e483f
vickyesantillan <vesantillan@live.com>
2021-05-10 16:54:58 -0300

    adding js function to the second img

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-var range = 6 * Math.random() + 1; // randoms numbers from 1.000-5.99999
-var randomNumber1 = Math.floor(range); //randoms numbers from 1-6
+//Setting the first img
+
+var range1 = 6 * Math.random() + 1; // randoms numbers from 1.000-5.99999
+var randomNumber1 = Math.floor(range1); //randoms numbers from 1-6
 //var imgRandomPath1To6 = 'images/dice' + randomNumber1 + '.png';
+
+//Setting the second img
+
+var range2 = 6 * Math.random() + 1;
+var randomNumber2 = Math.floor(range2);
 
 // document.addEventListener('click',document.querySelector('.img1'))
 
 document
   .querySelector('.img1')
   .setAttribute('src', 'images/dice' + randomNumber1 + '.png');
+
+document
+  .querySelector('.img2')
+  .setAttribute('src', 'images/dice' + randomNumber2 + '.png');
