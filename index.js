edbb89db93d92477ed95f6e2c9df8329c04d7a0c
vickyesantillan <vesantillan@live.com>
2021-05-11 10:35:25 -0300

    changing dice.html to index.html name

--- index.js
+++ index.js
@@ -11,10 +11,25 @@ var randomNumber2 = Math.floor(range2);
 
 // document.addEventListener('click',document.querySelector('.img1'))
 
+//Selecting the first img
 document
   .querySelector('.img1')
   .setAttribute('src', 'images/dice' + randomNumber1 + '.png');
 
+//Selecting the second img
 document
   .querySelector('.img2')
   .setAttribute('src', 'images/dice' + randomNumber2 + '.png');
+
+function winner() {
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector('h1').textContent = '🚩 Player 1 Wins!';
+  }
+  if (randomNumber1 < randomNumber2) {
+    document.querySelector('h1').textContent = 'Player 2 wins 🚩';
+  }
+  if (randomNumber1 === randomNumber2) {
+    document.querySelector('h1').textContent = 'Draw 🏳';
+  }
+}
+winner();
